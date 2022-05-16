e0a5de3779f498a53bd322eeeaf0df4492d89b4f
lyf <lyf1996zzz@163.com>
2022-05-16 17:56:02 +0800

    添加错误提示对话框。 2022/5/16 17：55

--- logic/classify_image.py
+++ logic/classify_image.py
@@ -101,7 +101,6 @@ class MainWindow(QtWidgets.QMainWindow, classifier_window.Ui_MainWindow):
         self.model.load_state_dict(torch.load(self.modelPath, map_location=torch.device('cpu')))
         self.image_path = None
         self.result = None
-
         self.pushButton.clicked.connect(self.open_image)
         self.pushButton_2.clicked.connect(self.classify_image)
         self.pushButton_3.clicked.connect(self.save_record)
@@ -122,28 +121,36 @@ class MainWindow(QtWidgets.QMainWindow, classifier_window.Ui_MainWindow):
             self.label_2.setScaledContents(True)  # 自适应图片大小
 
     def classify_image(self):
-        image = Image.open(self.image_path)
-        resized_image = image.resize((224, 224), Image.ANTIALIAS)
-        image_numpy = np.array(resized_image)
-        image_torch = torch.from_numpy(image_numpy)
-        image_torch = image_torch[None, :]
-        image_torch = image_torch.float()
-        image_torch = image_torch.permute(0, 3, 1, 2)
-        output = self.model(image_torch)
-        _, result = torch.max(output, 1)
-        if result == 0:
-            self.result = "合格"
-            self.label_3.setText("合格")
+        if self.image_path == None:
+            QtWidgets.QMessageBox.warning(None, '警告', '尚未上传图片', QtWidgets.QMessageBox.Ok)
         else:
-            self.result = "不合格"
-            self.label_3.setText("不合格")
+            image = Image.open(self.image_path)
+            resized_image = image.resize((224, 224), Image.ANTIALIAS)
+            image_numpy = np.array(resized_image)
+            image_torch = torch.from_numpy(image_numpy)
+            image_torch = image_torch[None, :]
+            image_torch = image_torch.float()
+            image_torch = image_torch.permute(0, 3, 1, 2)
+            output = self.model(image_torch)
+            _, result = torch.max(output, 1)
+            if result == 0:
+                self.result = "合格"
+                self.label_3.setText("合格")
+            else:
+                self.result = "不合格"
+                self.label_3.setText("不合格")
 
     def save_record(self):
-        path_list = self.image_path.split('/')
-        image_name = path_list[-3] + '/' + path_list[-2] + '/' + path_list[-1]
-        one_img_info = [datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), image_name, self.result]
-        with open('X:\金相研磨图像识别项目\pythonProject\\results_record\\results.pickle', 'ab') as file:
-            pickle.dump(one_img_info, file, 1)
+        if self.image_path != None and self.result != None:
+            path_list = self.image_path.split('/')
+            image_name = path_list[-3] + '/' + path_list[-2] + '/' + path_list[-1]
+            one_img_info = [datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), image_name, self.result]
+            with open('X:\金相研磨图像识别项目\pythonProject\\results_record\\results.pickle', 'ab') as file:
+                pickle.dump(one_img_info, file, 1)
+        elif self.image_path != None and self.result == None:
+            QtWidgets.QMessageBox.warning(None, '警告', '尚未对此图片进行识别', QtWidgets.QMessageBox.Ok)
+        else:
+            QtWidgets.QMessageBox.warning(None, '警告', '尚未上传图片', QtWidgets.QMessageBox.Ok)
 
     def back(self):
         pass
