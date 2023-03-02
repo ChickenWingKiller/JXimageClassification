e263e085290ecb5c8cd2f06733e02208710ff591
lyf <lyf1996zzz@163.com>
2023-03-02 21:46:41 +0800

    修改显示结果的界面，显示四列，隐藏默认序号列 2023/3/2

--- logic/show_records.py
+++ logic/show_records.py
@@ -1,3 +1,5 @@
+import sys
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 from UI import show_records
 import pickle
@@ -15,15 +17,27 @@ class MainWindow(QtWidgets.QMainWindow, show_records.Ui_MainWindow):
                 except:
                     break
         self.tableWidget.setRowCount(len(records_list))
-        self.tableWidget.setColumnCount(3)
-        self.tableWidget.setHorizontalHeaderLabels(['时间', '图片名', '结果'])
+        self.tableWidget.verticalHeader().setHidden(True)
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.setHorizontalHeaderLabels(['序号','时间', '图片名', '结果'])
         for i in range(len(records_list)):
-            for j in range(3):
-                data = QtWidgets.QTableWidgetItem(str(records_list[i][j]))
+            for j in range(4):
+                if (j==0):
+                    num = QtWidgets.QTableWidgetItem(str(i+1))
+                    num.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+                    self.tableWidget.setItem(i,j,num)
+                    continue
+                data = QtWidgets.QTableWidgetItem(str(records_list[i][j-1]))
+                if (j==3):
+                    data.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
                 self.tableWidget.setItem(i, j, data)
         self.tableWidget.resizeColumnToContents(0)  # 使列宽跟随内容改变
         self.tableWidget.resizeColumnToContents(1)  # 使列宽跟随内容改变
         self.tableWidget.setAlternatingRowColors(True)  # 使表格颜色交错显示
+        self.tableWidget.setColumnWidth(0, 80)
+        self.tableWidget.setColumnWidth(1, 210)
+        self.tableWidget.setColumnWidth(2, 300)
+        self.tableWidget.setColumnWidth(3, 160)
         self.pushButton.clicked.connect(self.back)
 
     def setui(self,window):
@@ -37,16 +51,35 @@ class MainWindow(QtWidgets.QMainWindow, show_records.Ui_MainWindow):
                 except:
                     break
         self.tableWidget.setRowCount(len(records_list))
-        self.tableWidget.setColumnCount(3)
-        self.tableWidget.setHorizontalHeaderLabels(['时间', '图片名', '结果'])
+        self.tableWidget.verticalHeader().setHidden(True)
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.setHorizontalHeaderLabels(['序号', '时间', '图片名', '结果'])
         for i in range(len(records_list)):
-            for j in range(3):
-                data = QtWidgets.QTableWidgetItem(str(records_list[i][j]))
+            for j in range(4):
+                if (j == 0):
+                    num = QtWidgets.QTableWidgetItem(str(i + 1))
+                    num.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+                    self.tableWidget.setItem(i, j, num)
+                    continue
+                data = QtWidgets.QTableWidgetItem(str(records_list[i][j - 1]))
+                if (j == 3):
+                    data.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
                 self.tableWidget.setItem(i, j, data)
         self.tableWidget.resizeColumnToContents(0)  # 使列宽跟随内容改变
         self.tableWidget.resizeColumnToContents(1)  # 使列宽跟随内容改变
         self.tableWidget.setAlternatingRowColors(True)  # 使表格颜色交错显示
+        self.tableWidget.setColumnWidth(0, 80)
+        self.tableWidget.setColumnWidth(1, 210)
+        self.tableWidget.setColumnWidth(2, 300)
+        self.tableWidget.setColumnWidth(3, 160)
         self.pushButton.clicked.connect(self.back)
 
     def back(self):
         pass
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyle("Fusion")
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
